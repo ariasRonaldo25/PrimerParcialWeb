cbc2b4cf75eca730573767fe1f1501edda9f75ef
Ronaldo Arias Parada <56136740+ariasRonaldo25@users.noreply.github.com>
2021-04-15 07:33:33 -0500

    agregando el endpoint cargado en git.
    ajustando el json de tecnologia.js

--- js/tecnologia.js
+++ js/tecnologia.js
@@ -1,5 +1,5 @@
 function cargarJson() {
-    var url = "http://demo6497253.mockable.io/categoria/tecnologia";
+    var url = "https://carlosreneas.github.io/endpoints/categoria_tecnologia.json";
 
     fetch(url)
         .then(response => response.json())
